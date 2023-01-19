d02c8d1820e6f0be8f5f92cc9ad465b544a0d5b6
MiguelDeSaAdegas <miguel.desaadegas@student.vinci.be>
2023-01-19 20:28:33 +0100

    style(services): pretier didn't like my code

--- src/services/__tests__/excludeNamesWithX.test.js
+++ src/services/__tests__/excludeNamesWithX.test.js
@@ -1,4 +1,4 @@
-const excludeNamesWithX = require('../excludeNamesWithX');
+const excludeNamesWithX = require("../excludeNamesWithX");
 
 describe("excludeNamesWithX tests suites", () => {
     test("should return an array", () => {
@@ -6,18 +6,18 @@ describe("excludeNamesWithX tests suites", () => {
         expect(result).toEqual([]);
     });
 
-    test('should return an array with all names as no names have an "x" letter' , () => {
-        const result = excludeNamesWithX(['Momo','Hadji','Leo']);
-        expect(result).toEqual(['Momo','Hadji','Leo']);
+    test('should return an array with all names as no names have an "x" letter', () => {
+        const result = excludeNamesWithX(["Momo", "Hadji", "Leo"]);
+        expect(result).toEqual(["Momo", "Hadji", "Leo"]);
     });
 
     test('should return an array with only the names that do not have the letter "x"', () => {
-        const result = excludeNamesWithX(['DMX','Xzibit','JeanKevin']);
-        expect(result).toEqual(['JeanKevin']);
+        const result = excludeNamesWithX(["DMX", "Xzibit", "JeanKevin"]);
+        expect(result).toEqual(["JeanKevin"]);
     });
 
-    test('should return an array with only the 2 names who do not have a x', () => {
-        const result = excludeNamesWithX(['JC','Leo','xena']);
-        expect(result).toEqual(['JC','Leo']);
+    test("should return an array with only the 2 names who do not have a x", () => {
+        const result = excludeNamesWithX(["JC", "Leo", "xena"]);
+        expect(result).toEqual(["JC", "Leo"]);
     });
 });
